380a34af79878ed109ce6d51e937af1316165d6c
Dan Leehr <dan.leehr@duke.edu>
2015-07-29 15:08:03 -0400

    Remove docker subdir

--- day2-sect8_querying_rdbms/docker/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start database container
-docker run -d -P --name movielens-db postgres:9.4.4
-
-#Load data
-docker build -t movielens-loader .
-docker run --rm --link movielens-db:db movielens-loader
-
